c990ae6109ec5027091868229eeabb1eef67df76
nunusmohum <halojw301@gmail.com>
2022-10-11 22:33:22 +0900

    cats, users module add

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,16 +2,12 @@ import { Body, Controller, Get, Param, Req } from '@nestjs/common';
 import { Request } from 'express';
 import { AppService } from './app.service';
 
-@Controller('cats')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('hello/:id/:name')
-  getHello(
-    @Req() req: Request,
-    @Body() body,
-    @Param() param: { id: string; name: string },
-  ): string {
+  @Get()
+  getHello(): string {
     return this.appService.getHello();
   }
 }
